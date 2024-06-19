b27eef33110f3e4d8afebf030eb533db0e41764a
Moraar222 <moraar976@gmail.com>
2024-06-19 14:00:24 +0300

    updated

---
+++ app/Featured/Feature.tsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import Feature from '@/components/feature';
+
+export class FeatureComponent extends PureComponent {
+  render() {
+    return (
+      <Feature />
+    );
+  }
+}
+
+export default FeatureComponent;
