7c0821b0376da7d62937543e9521a7d7c61a8faa
Moraar222 <moraar976@gmail.com>
2024-06-24 12:35:18 +0300

    updated

---
+++ Pages/auth/forgetpassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function forgetpassword() {
+  return (
+    <div>forgetpassword</div>
+  )
+}
+
+export default forgetpassword
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Navbar from '@/components/navbar'
-import Hero from '@/components/hero'
-import Footer from '@/components/footer'
+import React from 'react';
+import Navbar from '@/components/navbar';
+import Hero from '@/components/hero';
+import Footer from '@/components/footer';
+import Login from '@/Pages/auth/login';
 
-function page() {
+function Page() {
   return (
     <>
-    {/* <Navbar /> */}
-    <Hero />
-    <Footer/></>
-  )
+      {/* Uncomment if Navbar is needed */}
+      {/* <Navbar /> */}
+      <Hero />
+      <Footer />
+      <Login />
+    </>
+  );
 }
 
-export default page
+export default Page;
